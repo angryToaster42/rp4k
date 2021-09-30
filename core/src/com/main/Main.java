2585e5441878ecbb506e82f2bb40e482ad8a521a
AngryToaster42 <danny.kovachev@gmail.com>
2021-09-29 20:59:34 -0400

    added zombie animations

--- core/src/com/main/Main.java
+++ core/src/com/main/Main.java
@@ -5,27 +5,47 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main extends ApplicationAdapter {
+	// GAME VARIABLES
 	SpriteBatch batch;
-	Texture img;
-	
+	Random r;
+
+	// CONTROL VARIABLES
+
+	// GAME LISTS
+	ArrayList<Zombie> zombies = new ArrayList<Zombie>();
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		r = new Random();
+
+		for(int i = 0; i < 1000; i++) {
+			zombies.add(new Zombie("zzz", i * 20 + 526, r.nextInt(450), 1));
+		}
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+		//update();
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(Resources.bg, 0, 0);
+		for(Zombie z : zombies) z.draw(batch);
+		for(Zombie z : zombies) z.update();
 		batch.end();
 	}
-	
+
+	//void update() {
+		//zombie.update();
+	//}
+
+	// dont write anything bellow
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
